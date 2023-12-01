04bd7cfefb464fda57c208aba1ff406cbe42db9c
DinaMechergui <Dinamechergui7@gmail.com>
2023-12-01 22:05:59 +0100

    8eme commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:projet1/pages/detail_page.dart';
+import 'package:projet1/pages/home_page.dart';
 import 'package:projet1/pages/welcome_page.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailPage(),
+      home: HomePage(),
     );
   }
 }
